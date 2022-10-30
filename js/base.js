ed811654906075a3a28376b2c3b2cacd8a8e913f
Mandeep Singh <mandeep88712@gmail.com>
2022-10-31 03:21:51 +0530

    theme changed

--- js/base.js
+++ js/base.js
@@ -25,22 +25,3 @@ function scrollPage() {
     scrollStarted = false;
   }
 }
-
-var i=0;
-function navfunc() {
-    i++;
-    if (i%2==1 || document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      navbar.classList.add('nav-active');
-    } else {
-      navbar.classList.remove('nav-active');
-    }
-}
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-      navbar.classList.add('nav-active');
-    } else {
-      navbar.classList.remove('nav-active');
-    }
-  }
